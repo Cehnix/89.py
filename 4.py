a0318b2c2c414683f18aa828ebbb5c5be60f3398
Cehnix <56318178+Cehnix@users.noreply.github.com>
2019-10-08 16:27:28 -0300

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,8 @@
+letra = input("Escriba una letra: ")
+
+if(letra == "a") or (letra == "e") or (letra == "i") or (letra == "o") or (letra == "u"):
+    print(letra," es una vocal")
+else:
+    print(letra, " es una consonante")
+
+
